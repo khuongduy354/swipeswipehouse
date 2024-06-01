3192562bceabf873c572a63694df31edf72f820b
khuongduy354 <42113313+khuongduy354@users.noreply.github.com>
2024-06-01 23:20:04 +0700

    chore: setup supabase sdk

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
-import express from "express"  
+import express from "express";
+import "dotenv/config";
 
-const app = express(); 
-const PORT = 8000 
-
+const app = express();
+const PORT = 8000;
 
 app.get("/", (req, res) => {
   res.send("Hello World");
-}); 
-
+});
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
